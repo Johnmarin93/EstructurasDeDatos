cda2b7efb55fd385148de096e8698b9917bfc2b0
Johnmarin93 <john.marin@utp.edu.co>
2017-02-25 00:31:23 -0500

    Signed-off-by: Johnmarin93 <john.marin@utp.edu.co>

---
+++ src/estructurasdedatos/estructuraobjeto/Animal.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.estructuraobjeto;
+
+/**
+ *
+ * @author jhon_
+ */
+public class Animal {
+    private float Peso;
+    private String Raza;
+    private String Color;
+    private String Procedencia;
+    private String tipoanimal;
+    private int edad;
+
+    /**
+     * @return the Peso
+     */
+    public float getPeso() {
+        return Peso;
+    }
+
+    /**
+     * @param Peso the Peso to set
+     */
+    public void setPeso(float Peso) {
+        this.Peso = Peso;
+    }
+
+    /**
+     * @return the Raza
+     */
+    public String getRaza() {
+        return Raza;
+    }
+
+    /**
+     * @param Raza the Raza to set
+     */
+    public void setRaza(String Raza) {
+        this.Raza = Raza;
+    }
+
+    /**
+     * @return the Color
+     */
+    public String getColor() {
+        return Color;
+    }
+
+    /**
+     * @param Color the Color to set
+     */
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    /**
+     * @return the Procedencia
+     */
+    public String getProcedencia() {
+        return Procedencia;
+    }
+
+    /**
+     * @param Procedencia the Procedencia to set
+     */
+    public void setProcedencia(String Procedencia) {
+        this.Procedencia = Procedencia;
+    }
+
+    /**
+     * @return the tipoanimal
+     */
+    public String getTipoanimal() {
+        return tipoanimal;
+    }
+
+    /**
+     * @param tipoanimal the tipoanimal to set
+     */
+    public void setTipoanimal(String tipoanimal) {
+        this.tipoanimal = tipoanimal;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
